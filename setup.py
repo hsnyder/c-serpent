7bd4c66969c62ddc6634d8ae2b74a48232558a1f
Harris M Snyder <harris.snyder@gmail.com>
2024-03-09 15:52:36 -0500

    bump version number, fix missing import in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, Extension, find_packages
 from setuptools.command.build_py import build_py
 from setuptools.command.install import install
 import subprocess
-import os
+import os, sys
 
 class CustomBuild(build_py):
     def run(self):
@@ -10,13 +10,18 @@ class CustomBuild(build_py):
         if not self.dry_run:
             setup_dir = os.path.dirname(os.path.abspath(__file__))
             cserpent_path = os.path.join(setup_dir, 'cserpent.c')
-            subprocess.check_call(['cc', cserpent_path, '-o', 'cserpent'])
+            if sys.platform == 'win32':
+                binary_name = 'cserpent.exe'
+                subprocess.check_call(['cl', cserpent_path, '/Fe:', binary_name])
+            else:
+                binary_name = 'cserpent'
+                subprocess.check_call(['cc', cserpent_path, '-o', binary_name])
             build_lib = os.path.abspath(self.build_lib)
-            self.copy_file('cserpent', build_lib)
+            self.copy_file(binary_name, build_lib)
 
 setup(
     name='cserpent',
-    version='1.1',
+    version='1.1.1',
     ext_modules=[Extension('cserpent_py', sources=['cserpent_py.c'])],
     cmdclass={ 'build_py': CustomBuild, },
     package_data={'cserpent': ['cserpent']},
